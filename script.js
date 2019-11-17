7c61e5609f45b11cfba55cadeb6a14268fb01279
jrd656 <51966598+jrd656@users.noreply.github.com>
2019-11-17 09:47:15 +0000

    Fix carousel issue
    Found that there was something going wrong with the play/pause function of my carousel. Changed space to p key for pause. Space kept moving page down and thought probably best avoided.

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function lastModified() {
 
 
 var myIndex = 0;
-var defaultInterval = 1500;
+var defaultInterval = 500;
 var slideInterval = setInterval(carousel,defaultInterval);
 
 
@@ -18,19 +18,19 @@ function carousel() {
   var i;
   var x = document.getElementsByClassName("mySlides"); // x = array of images
   var dots = document.getElementsByClassName("dot"); //round buttons as an array
-  var captions = document.getElementsByClassName("caption-text");
+  var captions = document.getElementsByClassName("caption-text"); //captions array
 
   for (i = 0; i < x.length; i++) { //Hides elements
     x[i].style.display = "none";  // Makes all images invisible
     dots[i].className = dots[i].className.replace(" JDwhite", ""); //makes dots all blank instead of white
-    captions[i].style.display = "none";
+    captions[i].style.display = "none"; //hides all captions
   }
 
   myIndex++; // Adds 1 to index, so now index is 1. Stops -1 being an answer.
   if (myIndex > x.length) {myIndex = 1} //Brings index back to 1 once index gets higher than length. 
-  if (myIndex <= 0) {myIndex = x.length
-} 
+  if (myIndex <= 0) {myIndex = x.length}; 
 
+//show the relevant item
   x[myIndex-1].style.display = "block";  //Displays the index image -1.
   dots[myIndex-1].className += " JDwhite"; // Makes dot solid.
   captions[myIndex-1].style.display = "block";
@@ -38,21 +38,34 @@ function carousel() {
 }
 
 //Arrow function  
-function plusDivs(arrowDirection){
+function plusDivs(arrowDirection){ // arrow direction gives arg -1 for left, 1 for right arrow
+    
+    //take arrays
     var x = document.getElementsByClassName("mySlides"); // x = array of images
-  var dots = document.getElementsByClassName("dot"); //round buttons as an array
+    var dots = document.getElementsByClassName("dot"); //round buttons as an array
+    var captions = document.getElementsByClassName("caption-text"); //captions array
 
-    for (i = 0; i < x.length; i++) {
+    for (i = 0; i < x.length; i++) { //hide arrays
     x[i].style.display = "none";  // Makes all images invisible
     dots[i].className = dots[i].className.replace(" JDwhite", ""); //makes dots all blank instead of white
-
+    captions[i].style.display = "none"; //hides all captions
 }
 
-myIndex += arrowDirection
+myIndex += arrowDirection // myIndex -1 or 1 depending on arrow direction.
 if (myIndex > x.length) {myIndex = 1} //Brings index back to 1 once index gets higher than length. 
 if (myIndex <= 0) {myIndex = x.length} 
+
+//show the relevant item
 x[myIndex-1].style.display = "block";  //Displays the index image -1.
   dots[myIndex-1].className += " JDwhite"; // Makes dot solid.
+  captions[myIndex-1].style.display = "block";
+
+  if(playing) {
+    pauseSlideshow();
+    playSlideshow();
+  //} else {
+    // playSlideshow();
+  }
 
 }
 
@@ -75,9 +88,10 @@ function playSlideshow() {
 pauseButton.onclick = function() {
     if(playing) {
     pauseSlideshow();
-  } else {
+    } 
+    else {
     playSlideshow();
-  }
+    }
 };
 
 //Key presses
@@ -85,19 +99,26 @@ pauseButton.onclick = function() {
 window.addEventListener("keydown",TestFunction,false);
 
 function TestFunction(key) {
-    //press space to pause
-    if (key.keyCode == 32) {
+    //press 'p' to pause
+    if (key.keyCode == 80) {
         if(playing) {pauseSlideshow()} 
-        else {playSlideshow();}
+        else {
+            pauseSlideshow()
+            playSlideshow();
+        }
     }
     //press left or right
     if (key.keyCode == 37) {plusDivs(-1)
-        clearInterval(slideInterval);
-        slideInterval = setInterval(carousel,defaultInterval);}
-    if (key.keyCode == 39) {plusDivs(1)}
+        // clearInterval(slideInterval);
+        // slideInterval = setInterval(carousel,defaultInterval);
+    }
+
+    if (key.keyCode == 39) {plusDivs(1)
+    
+    }
 }
 
-//space is 32
+//space is 32, p is 80
 //left arrow 37 right arrow 38
 
 
